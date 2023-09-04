b8d1d8cff624b6896be0a90ca240c993f42e800c
aswin <aswin0484@gmail.com>
2023-09-04 13:33:16 +0530

    initial commit

---
+++ loginapp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  loginapp
+//
+//  Created by ASWIN A on 04/09/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var passwordLbl: UILabel!
+    @IBOutlet weak var emailLbl: UILabel!
+    
+    var email = ""
+    var password = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        emailLbl.text = "Email: \(email)"
+//        passwordLbl.text = "Password: \(password)"
+    }
+
+}
--- loginapp/ViewController.swift
+++ loginapp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var psw: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func signin(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "secondViewController") as! SecondViewController
+        
+//        vc.email = email.text!
+//        vc.password = psw.text!
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
-
